8dbe4fb1beb7d7ca84c5f73ad6173a6573b1af61
Moody <76251897+moodyhunter@users.noreply.github.com>
2021-06-12 23:00:41 +0800

    + submodule and include cleanup

--- include/Qv2rayBase/Qv2rayBaseFeatures.hpp
+++ include/Qv2rayBase/Qv2rayBaseFeatures.hpp
@@ -1,16 +1,7 @@
 #pragma once
 
-// clang-format off
-    #define QVFEATURE_subscriptions 1
-
-#ifndef QVFEATURE_statistics
-    #define QVFEATURE_statistics                     1
-#endif
-
-#ifndef QVFEATURE_latency
-    #define QVFEATURE_latency                        -1
-#endif
-
-// clang-format on
+#define QVFEATURE_subscriptions 1
+#define QVFEATURE_statistics 1
+#define QVFEATURE_latency 1
 
 #define QV2RAYBASE_FEATURE(feat) ((1 / QVFEATURE_##feat) == 1)
