6f55edc833d0ba70aeff58f5ecd43aa63861a12a
Moody <moodyhunter@users.noreply.github.com>
2021-06-29 23:41:22 +0800

    + fixed up installation, added USE_SYSTEM_UVW

--- cmake/Deployment.cmake
+++ cmake/Deployment.cmake
@@ -1,38 +1,45 @@
 generate_export_header(Qv2rayBase
     EXPORT_FILE_NAME ${CMAKE_CURRENT_BINARY_DIR}/Qv2rayBase_export.h)
 
-configure_package_config_file(cmake/Qv2rayBase.cmake.in
-  ${CMAKE_CURRENT_BINARY_DIR}/Qv2rayBaseConfig.cmake
-  INSTALL_DESTINATION ${LIB_INSTALL_DIR}/cmake/Qv2rayBase)
+configure_package_config_file(cmake/Qv2rayBase.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/Qv2rayBaseConfig.cmake
+    INSTALL_DESTINATION ${LIB_INSTALL_DIR}/cmake/Qv2rayBase)
 
-write_basic_package_version_file(
-  ${CMAKE_CURRENT_BINARY_DIR}/Qv2rayBaseConfigVersion.cmake
-  COMPATIBILITY SameMajorVersion)
+write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/Qv2rayBaseConfigVersion.cmake
+    COMPATIBILITY SameMajorVersion)
 
 install(TARGETS Qv2rayBase
     EXPORT Qv2rayBaseTargets
+    COMPONENT "development"
+    EXCLUDE_FROM_ALL
     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
 )
 
-export(EXPORT Qv2rayBaseTargets
-    FILE "${CMAKE_CURRENT_BINARY_DIR}/cmake/Qv2rayBaseTargets.cmake"
-    NAMESPACE Qv2ray::
-)
+# Some development required files.
+
+# BEGIN Include Headers
+install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/
+    COMPONENT "development"
+    EXCLUDE_FROM_ALL
+    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/Qv2rayBase_export.h
     COMPONENT "development"
     EXCLUDE_FROM_ALL
-    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/Qv2rayBase/)
+    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/Qv2rayBase)
+# END Include Headers
 
+
+# BEGIN Target Support Files
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/Qv2rayBaseConfig.cmake ${CMAKE_CURRENT_BINARY_DIR}/Qv2rayBaseConfigVersion.cmake
     COMPONENT "development"
     EXCLUDE_FROM_ALL
     DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/Qv2rayBase)
 
-install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/
-    COMPONENT "development"
-    EXCLUDE_FROM_ALL
-    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+export(EXPORT Qv2rayBaseTargets
+    FILE "${CMAKE_CURRENT_BINARY_DIR}/cmake/Qv2rayBaseTargets.cmake"
+    NAMESPACE Qv2ray::
+)
 
 install(EXPORT Qv2rayBaseTargets
     COMPONENT "development"
@@ -41,3 +48,4 @@ install(EXPORT Qv2rayBaseTargets
     NAMESPACE Qv2ray::
     DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/Qv2rayBase
 )
+# END
