b7744f4d429d3cbe5d19bd30bd5dfe6ff1b97a51
Moody <76251897+moodyhunter@users.noreply.github.com>
2021-06-30 20:25:34 +0800

    - removed EXCLUDE_FROM_ALL from Qv2rayBase

--- cmake/Windows.cmake
+++ cmake/Windows.cmake
@@ -8,13 +8,14 @@ generate_product_version(
     VERSION_MINOR      ${PROJECT_VERSION_MINOR}
     VERSION_PATCH      ${PROJECT_VERSION_PATCH}
     VERSION_REVISION   ${PROJECT_VERSION_TWEAK}
-    COMPANY_NAME       "Qv2ray Workgroup"
-    COMPANY_COPYRIGHT  "Qv2ray Workgroup 2021"
+    COMPANY_NAME       "Moody"
+    COMPANY_COPYRIGHT  "Moody 2021"
     FILE_DESCRIPTION   "Qv2ray Base Library"
     )
 add_definitions(-DUNICODE -D_UNICODE -DNOMINMAX)
 
 if(NOT MINGW)
+    set(CMAKE_DEBUG_POSTFIX d)
     add_compile_options("/utf-8")
     add_definitions(-D_WIN32_WINNT=0x600 -D_SCL_SECURE_NO_WARNINGS -D_CRT_SECURE_NO_WARNINGS -DNOMINMAX)
 endif()
